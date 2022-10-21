761494cdfe27301a3b085e1f4f47b17e98e458cf
hunaglulu <hll10120212@sina.com>
2022-10-21 19:43:57 +0800

    improvement-OPEN feign配置

--- fairy-cloud-auth/fairy-authorication-server/src/main/java/com/faity/auth/authorication/provider/FeignConfig.java
+++ fairy-cloud-auth/fairy-authorication-server/src/main/java/com/faity/auth/authorication/provider/FeignConfig.java
@@ -1,6 +1,11 @@
 package com.faity.auth.authorication.provider;
 
 import feign.Logger;
+import feign.Request;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,5 +19,19 @@ public class FeignConfig {
     @Bean
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.BASIC;
+    }
+
+    @Bean
+    public Request.Options options() {
+        return new Request.Options(5000, 10000);
+    }
+
+    @Bean
+    public Decoder decoder() {
+        return new JacksonDecoder();
+    }
+    @Bean
+    public Encoder encoder() {
+        return new JacksonEncoder();
     }
 }
