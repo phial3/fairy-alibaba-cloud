93494174b478773184117b91956b7a537a8c376f
hunaglulu <hll10120212@sina.com>
2022-10-30 16:49:29 +0800

    pom

--- fairy-cloud-mbg/src/main/java/com/fairy/cloud/mbg/mapper/OmsOrderItemMapper.java
+++ fairy-cloud-mbg/src/main/java/com/fairy/cloud/mbg/mapper/OmsOrderItemMapper.java
@@ -3,6 +3,7 @@ package com.fairy.cloud.mbg.mapper;
 import com.fairy.cloud.mbg.model.pojo.OmsOrderItemPO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * 订单中所包含的商品
@@ -12,6 +13,7 @@ import org.apache.ibatis.annotations.Mapper;
  * @date 2022-08-02 21:00:43
  */
 @Mapper
+@Repository
 public interface OmsOrderItemMapper extends BaseMapper<OmsOrderItemPO> {
 	
 }
