4099a427543492752dbb4c1888e8fec36b6e410b
hunaglulu <hll10120212@sina.com>
2022-11-01 22:36:29 +0800

    基本信息

--- fairy-cloud-common/src/main/java/com/fairy/common/entity/po/BasePo.java
+++ fairy-cloud-common/src/main/java/com/fairy/common/entity/po/BasePo.java
@@ -2,6 +2,8 @@ package com.fairy.common.entity.po;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -12,20 +14,25 @@ import java.util.Date;
  * @date 2022/10/13 20:22
  */
 @Data
+@ApiModel("")
 public class BasePo implements Serializable {
 
     @TableField(exist = false)
     public static final String DEFAULT_USERNAME = "system";
 
+    @ApiModelProperty(value = "创建者")
     @TableField(fill = FieldFill.INSERT)
     private String creator;
 
+    @ApiModelProperty(value = "创建时间")
     @TableField(fill = FieldFill.INSERT)
     private Date createdTime;
 
+    @ApiModelProperty(value = "更新者")
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private String updator;
 
+    @ApiModelProperty(value = "更新时间")
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updatedTime;
 }
--- fairy-cloud-common/src/main/java/com/fairy/common/error/ServiceException.java
+++ fairy-cloud-common/src/main/java/com/fairy/common/error/ServiceException.java
@@ -1,11 +1,13 @@
 package com.fairy.common.error;
 
 import com.fairy.common.enums.ErrorType;
+import com.fairy.common.enums.ServiceErrorEnum;
 import com.fairy.common.enums.SystemErrorEnum;
 import lombok.Getter;
 
 /**
  * 业务异常
+ *
  * @author 鹿少年
  * @date 2022/10/13 21:06
  */
@@ -26,6 +28,11 @@ public class ServiceException extends RuntimeException {
     public ServiceException(ErrorType errorType, String message) {
         super(message);
         this.errorType = errorType;
+    }
+
+    public ServiceException(String message) {
+        super(message);
+        this.errorType = ServiceErrorEnum.SERVICE_ERROR;
     }
 
     public ServiceException(ErrorType errorType, String message, Throwable cause) {
